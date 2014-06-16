764f5532e1bed769d85dac40b3e91d342c30b739
Ripu Singla <singlaripu@gmail.com>
2014-06-17 00:21:57 +0530

    Initial Commit

---
+++ www/js/services.js
@@ -0,0 +1,24 @@
+angular.module('starter.services', [])
+
+/**
+ * A simple example service that returns some data.
+ */
+.factory('Friends', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var friends = [
+    { id: 0, name: 'Monika' },
+    { id: 1, name: 'Rachel' }
+  ];
+
+  return {
+    all: function() {
+      return friends;
+    },
+    get: function(friendId) {
+      // Simple index lookup
+      return friends[friendId];
+    }
+  }
+});
